d1e0c4d1a28e0561b2919eb81d27c2f62def5af2
nyanye <iam@nyanye.com>
2017-01-20 13:00:12 +0900

    Files added

---
+++ goorm/__init__.py
@@ -0,0 +1,7 @@
+__title__ = 'Goorm'
+__version__ = '0.1.1'
+__author__ = 'Taylor Jung'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Taylor Jung'
+
+from . import tag
---
+++ goorm/goorm/__init__.py
@@ -0,0 +1,4 @@
+from .wordcloud import WordCloud, random_color_func, get_single_color_func
+from .color_from_image import ImageColorGenerator
+
+__all__ = ['WordCloud', 'random_color_func', 'get_single_color_func', 'ImageColorGenerator']
